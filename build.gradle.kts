81f2a2db64438d372c03c43232038174d2356603
이상민 <sangminlee7648@gmail.com>
2024-09-30 19:46:54 +0900

    week1 과제 간단 수정  (#1)
    * 불필요한 의존성 제거
    
    * spring dependency management 플러그인 gradle bom platform으로 대체

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,8 +4,6 @@ plugins {
 	kotlin("jvm") version "1.8.22"
 	kotlin("plugin.spring") version "1.8.22"
 	id("org.springframework.boot") version "2.7.18"
-	id("io.spring.dependency-management") version "1.1.6"
-
 	id("com.google.cloud.tools.jib") version "3.3.2"
 }
 
@@ -23,20 +21,19 @@ repositories {
 }
 
 dependencies {
+	implementation(platform(org.springframework.boot.gradle.plugin.SpringBootPlugin.BOM_COORDINATES))
+
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib")
 
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
-	testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
-	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
 tasks.withType<KotlinCompile> {
 	kotlinOptions {
 		freeCompilerArgs = listOf("-Xjsr305=strict")
-		jvmTarget = "17"
 	}
 }
 
