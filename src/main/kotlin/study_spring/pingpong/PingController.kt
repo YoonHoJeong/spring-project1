34833b314d496707d91a8f8acf8f5ddbbb3258cf
yoonhojeong <uno8941@gmail.com>
2024-10-03 15:55:11 +0900

    add: ping api using @Value

--- src/main/kotlin/study_spring/pingpong/PingController.kt
+++ src/main/kotlin/study_spring/pingpong/PingController.kt
@@ -1,15 +1,18 @@
 package study_spring.pingpong
 
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.web.bind.annotation.RestController
 import org.springframework.web.bind.annotation.GetMapping
 
 
 @RestController
 class PingController {
+    @Value("\${spring.pong.value:init_1}")
+    var rtnValue: String = "init_0"
 
     @GetMapping("/ping")
     fun ping(): String {
-        return "pong"
+        return rtnValue
     }
 
 }
--- src/main/kotlin/study_spring/pingpong/PingpongApplication.kt
+++ src/main/kotlin/study_spring/pingpong/PingpongApplication.kt
@@ -1,8 +1,16 @@
 package study_spring.pingpong
 
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.ComponentScan
 
+/*
+* @SpringBootApplication 어노테이션을 통해 아래 어노테이션이 자동으로 설정된다.
+* @Configuration
+* @EnableAutoConfiguration
+* @ComponentScan
+* */
 @SpringBootApplication
 open class PingpongApplication
 
