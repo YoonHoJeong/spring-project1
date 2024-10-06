41d550443b81554efd74ca2a1ee7ecaab93b029a
yoonhojeong <uno8941@gmail.com>
2024-10-06 12:50:42 +0900

    refactor: request Body, dto를 controller로 이동

--- src/main/kotlin/study_spring/pingpong/dto/GetLeaderBoardResponseDto.kt
+++
@@ -1,8 +0,0 @@
-package study_spring.pingpong.dto
-
-import study_spring.pingpong.game.model.GameRecord
-
-data class GetLeaderBoardResponseDto(
-    val avg: Double,
-    val maxScoreRecord: GameRecord?
-)
--- src/main/kotlin/study_spring/pingpong/game/controller/GameController.kt
+++ src/main/kotlin/study_spring/pingpong/game/controller/GameController.kt
@@ -1,20 +1,31 @@
 package study_spring.pingpong.game.controller
 
 import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.MethodArgumentNotValidException
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
-import study_spring.pingpong.dto.CreateRecordRequestDto
-import study_spring.pingpong.dto.GetLeaderBoardResponseDto
 import study_spring.pingpong.game.model.GameRecord
 import study_spring.pingpong.game.service.GameService
 import javax.validation.Valid
+import javax.validation.constraints.Min
+import javax.validation.constraints.NotNull
 
 @RestController
 @RequestMapping("/game")
 class GameController(private val gameService: GameService) {
+    data class CreateRecordRequestBody(
+        @field:Min(value = 0, message = "Score must be at least 0")
+        val score: Int
+    )
+
+    data class GetLeaderBoardResponseDto(
+        val avg: Double,
+        val maxScoreRecord: GameRecord?
+    )
+
     @GetMapping("/leaderboard")
     fun getLeaderBoard(): ResponseEntity<GetLeaderBoardResponseDto> {
         val avg = gameService.getAvgScore()
@@ -29,7 +40,7 @@ class GameController(private val gameService: GameService) {
     }
 
     @PostMapping("/save")
-    fun createRecord(@Valid @RequestBody request: CreateRecordRequestDto): ResponseEntity<GameRecord> {
+    fun createRecord(@Valid @RequestBody request: CreateRecordRequestBody): ResponseEntity<GameRecord> {
         val newRecord = gameService.createGameRecord(request.score)
         return ResponseEntity.ok(newRecord)
     }
